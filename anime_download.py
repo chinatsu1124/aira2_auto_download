92c9db0af43cf0cab0e54579dfd3d7d8b5958d81
chinatsu1124 <459293631@qq.com>
2022-08-08 17:42:42 +0800

    init

---
+++ anime_download.py
@@ -0,0 +1,20 @@
+import aria2p
+
+# initialization, these are the default values
+aria2 = aria2p.API(
+    aria2p.Client(
+        host="http://chinatsu1124.imwork.net",
+        port=6800,
+        secret="hcy1997912"
+    )
+)
+
+# list downloads
+downloads = aria2.get_downloads()
+
+for download in downloads:
+    print(download.name, download.download_speed)
+
+# http_uri = "https://img.alicdn.com/imgextra/i4/O1CN01sBgsI61EaaWZghWN9_!!6000000000368-2-tps-484-316.png"
+#
+# download = aria2.add(http_uri)
